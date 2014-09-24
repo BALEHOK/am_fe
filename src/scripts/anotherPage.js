cc067a447284aa1de86f8c80ff719a958b46f540
Ilya Bolkhovsky <ilya.bolkhovsky@721lab.com>
2014-09-24 16:57:20 +0400

    remove sample components

--- src/scripts/anotherPage.js
+++
@@ -1,6 +0,0 @@
-var React = require('react'),
-// Require the appropriate top level component
-  Page = require('./components/anotherPage/page'),
-  mountPoint = document.querySelector('body');
-
-React.renderComponent( Page(null) , mountPoint );
--- webpack.entries.js
+++ webpack.entries.js
@@ -1,5 +1,3 @@
 module.exports = {
-  index: './webpack_entries/index.js',
-  anotherPage: './webpack_entries/anotherPage.js',
   notes: './webpack_entries/notes.js'
 };
