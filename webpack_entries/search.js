0119eea09f2fd2e30015ed5639df8518a42e7fee
Alexey Saenko <saenko.alexei@gmail.com>
2014-10-04 21:15:41 +0400

    removed unused files

--- webpack_entries/search.js
+++
@@ -1,6 +0,0 @@
-var $ = require('jquery/dist/jquery.min.js');
-var _ = require('underscore/underscore-min.js');
-var Backbone = require('backbone/backbone-min.js');
-var React = require('react');
-
-require('../src/scripts/search.js');
