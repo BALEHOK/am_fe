67bba0065e016d470050be681f65e5b0642fce55
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2014-10-14 17:03:01 +0400

    simplify router

--- src/scripts/models/Router.ts
+++ src/scripts/models/Router.ts
@@ -5,47 +5,13 @@ export class Router extends Backbone.Router {
 
     constructor(session: session.SessionModel) {
         var routes: Backbone.RouterOptions = {
-            routes : {
-                '': 'search',
-                'tasks': 'tasks',
-                'categories': 'categories',
-                'documents': 'documents',
-                'financial': 'financial',
-                'reports': 'reports',
-                'reservations': 'reservations'
+            routes: {
+                '*actions': 'defaultAction' // All urls will trigger this route
             }
         }
         super(routes);
-        this.on("route", (route) => {
-            //console.log(route);
+        this.on("route:defaultAction", (route) => {
+            this.current = route;
         });
-    }
-
-    search() {
-        this.current = 'search';
-    }
-
-    tasks() {
-        this.current = 'tasks';
-    }
-
-    categories() {
-        this.current = 'categories';
-    }
-
-    documents() {
-        this.current = 'documents';
-    }
-
-    financial() {
-        this.current = 'financial';
-    }
-
-    reports() {
-        this.current = 'reports';
-    }
-
-    reservations() {
-        this.current = 'reservations';
     }
 }
