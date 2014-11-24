8c4a1e255dac1f2ef34fb1d8390b7cb0f8ce8036
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2014-11-24 13:52:32 +0300

    add dashboard and profile links

--- src/scripts/components/common/userNav.jsx
+++ src/scripts/components/common/userNav.jsx
@@ -12,9 +12,9 @@ var UserNav = React.createClass({
                 <a href="#" className="user-nav__pic">
                     <img src={this.props.user.userpicPath} alt=""/>
                 </a>
-                <a className="user-nav__profile-link" href="#">{this.props.user.userName}</a>
+                <a className="user-nav__profile-link" href="/MySettings.aspx">{this.props.user.userName}</a>
                 <span className="user-nav__actions">
-                    <a className="user-nav__actions-item user-nav__actions-item_icon_settings" href="#" title="Settings"></a>                    
+                    <a className="user-nav__actions-item user-nav__actions-item_icon_settings" href="/admin" title="Dashboard"></a>                    
                     <a className="user-nav__actions-item user-nav__actions-item_icon_logout" href="#" onClick={this.props.onLogout} title="Logout"></a>
                 </span>
                 <span className="user-nav__login-date">Last login: {this.props.user.lastLogin.format('HH:mm DD.MM.YYYY')}</span>
