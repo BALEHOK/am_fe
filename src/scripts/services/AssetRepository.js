ca990c17c5de21130e1ba7b255884d9a46228693
Termina1 <terminal2010@gmail.com>
2014-12-23 12:45:33 +0300

    commit repos

---
+++ src/scripts/services/AssetRepository.js
@@ -0,0 +1,12 @@
+class SearchRepository {
+    loadAsset(params) {
+        var url = `/api/assettype/${params.assetTypeUid}/asset/${params.assetUid}`;
+        return $.ajax({
+            url: url,
+            contentType: 'application/json',
+            type: 'GET'
+        });
+    }
+}
+
+module.exports = SearchRepository;
