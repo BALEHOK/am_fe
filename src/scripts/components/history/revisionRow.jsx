50d2c8c437ffc4b68fb01e6b2da478679e5c4149
Termina1 <terminal2010@gmail.com>
2015-02-22 16:05:24 +0300

    add user name and link to revision

--- src/scripts/components/history/revisionRow.jsx
+++ src/scripts/components/history/revisionRow.jsx
@@ -5,26 +5,31 @@
 var React = require('react');
 var moment = require('moment');
 var RevisionCell = require('./revisionCell.jsx');
+var Link = require('react-router').Link;
 
 var RevisionRow = React.createClass({
 
     render() {
         var revision = this.props.revision;
         var date = moment(revision.createdAt);
-        var cells = revision.changedValues.map(el => <RevisionCell cell={el} />)
+        var cells = revision.changedValues.map(el => <RevisionCell cell={el} />);
         return (
             <tr>
                 <td width="11%">{revision.revisionNumber}</td>
                 <td width="15%">
                     {date.format("DD.MM.YYYY")} <br/>
                     {date.format("hh:mm:ss A")} <br/>
-                    <a href="#">Admin</a>
+                    <a href="#">{revision.createdByUser.name}</a>
                 </td>
                 <td width="68%" colSpan="3">
                     {cells}
                 </td>
                 <td width="6%">
-                    <a className="link link_second" href="#"><span className="icon icon_eye"></span></a>
+                    <Link className="link link_second"
+                        to="asset-view"
+                        params={{assetTypeUid: revision.assetTypeUid, assetUid: revision.assetUid}}>
+                        <span className="icon icon_eye"></span>
+                    </Link>
                 </td>
             </tr>
         );
