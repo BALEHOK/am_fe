de9d9a13910c76822136d39cea4d006a5d67de01
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-03-16 16:37:09 +0700

    fix barcode image

--- src/scripts/components/asset/attributes/view/image.jsx
+++ src/scripts/components/asset/attributes/view/image.jsx
@@ -10,13 +10,18 @@ var Attribute = React.createClass({
         if(file.length > 14) {
             file = file.slice(0, 8) + "..." + file.slice(-3);
         }
+        var src = this.props.params.value;
+        if (this.props.params.data) {
+            var format = this.props.params.dataFormat || 'png';
+            src = `data:image/${format};base64,${this.props.params.data}`;
+        }
         return (
             <div className="asset-data__param">
                 <span className="asset-data__param-title">{this.props.params.name}:</span>
                 <div className="image-wrapper">
                     <div className="image-wrapper__subwrapper">
                         <div className="image-wrapper__img">
-                            <img src={this.props.params.value} alt={this.props.params.name} />
+                            <img src={src} alt={this.props.params.name} />
                         </div>
                     </div>
                     <div className="image-wrapper__name">{file}</div>
