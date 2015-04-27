1a9fe0542b00d5ba8aed364d0c07347323645b2d
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-04-27 13:41:35 +0300

    add file download url

--- src/scripts/components/asset/attributes/view/file.jsx
+++ src/scripts/components/asset/attributes/view/file.jsx
@@ -6,8 +6,15 @@ var React = require('react');
 var Url = require('./url')
 
 var Attribute = React.createClass({
+	contextTypes: {
+        router: React.PropTypes.func
+    },
     render: function() {
-        return <Url params={this.props.params}/>;
+    	var urlParams = this.context.router.getCurrentParams();
+    	var params = this.props.params;
+    	var url = `/FileHandler.ashx?assetTypeId=${urlParams.assetTypeId}&assetId=${urlParams.assetId}&attributeId=${params.id}`;
+    	params = _.extend(params, { url: url });
+        return <Url params={params} />;
     }
 });
 
