6773caa021de1b18f9374834090dc5170d072b70
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-04-27 13:56:49 +0300

    fix intendation

--- src/scripts/components/asset/attributes/view/barcode.jsx
+++ src/scripts/components/asset/attributes/view/barcode.jsx
@@ -7,7 +7,7 @@ var ImageAttribute = require('./image');
 var Flux = require('delorean').Flux;
 
 var Attribute = React.createClass({
-	  mixins:[Flux.mixins.storeListener],
+	mixins:[Flux.mixins.storeListener],
     render: function() {
     	var params = _.extend({}, this.props.params, {
     		data: this.state.stores.asset.asset.barcode
