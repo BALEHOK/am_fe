f1548a72736d973e8bd8b71c51e5c4fb90193fbf
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-06-16 20:02:57 +0300

    fix login url

--- src/scripts/mixins/AuthenticatedRouteMixin.js
+++ src/scripts/mixins/AuthenticatedRouteMixin.js
@@ -1,14 +1,11 @@
-﻿var LoginPage = require('../components/login/main.jsx');
+var LoginPage = require('../components/login/main.jsx');
 var AuthService = require('../services/AuthService.js');
 
 var AuthenticatedRouteMixin = {
     statics: {
         willTransitionTo: function (transition, params, query) {
             if (!window.app.tokenStore.getToken()) {
-                if (location.href.indexOf('localhost') > 0)
-                    transition.redirect('login', {}, {'nextPath' : transition.path});
-                else
-                    location.href = '/login';
+                transition.redirect('login', {}, {'nextPath' : transition.path});
             }
         }
     }
