4966ab865e656891e7b84ff68c5c354c93290a1b
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-07-15 19:58:51 +0300

    reports list

--- src/scripts/components/reports/reportsList.jsx
+++ src/scripts/components/reports/reportsList.jsx
@@ -15,11 +15,21 @@ var ReportsList = React.createClass({
         return (
                 <Loader loading={this.state.loading}>
                     <div>
-                        <h1>Reports list</h1>
-                        {this.state.stores.report.reports.map(r => {
-                            console.log(r)
-                            return <span />;
-                        })}
+                        <h1 className="page-title">Reports list</h1>
+                        <table>
+                            <tr>
+                                <th width="80%"></th>
+                                <th width="20%"></th>
+                            </tr>
+                            {this.state.stores.report.reports.map(r => {
+                                console.log(r)
+                                var url = APIURL + '/reports/' + r.id;
+                                return <tr>
+                                    <td><a target="_blank" href={url}>{r.name}</a></td>
+                                    <td></td>
+                                </tr>;
+                            })}
+                        </table>
                     </div>
                 </Loader>
             );
