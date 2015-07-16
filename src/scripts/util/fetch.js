4d828a97104fe6b51e78195a6f990166cbc72510
Termina1 <terminal2010@gmail.com>
2015-07-16 19:17:02 +0300

    fix jquery incompats

--- src/scripts/util/fetch.js
+++ src/scripts/util/fetch.js
@@ -2,17 +2,39 @@ import fetchival from "fetchival";
 import _ from "underscore";
 import LoginActions from "../actions/LoginActions";
 import {store as LoginStore} from "../stores/LoginStore";
+import {param} from "./util";
 
 export default function fetch(url, options = {}) {
-  let defOptions = {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      mode: 'cors'
-  };
-  if (LoginStore.access_token && url != '/token') {
-      defOptions.headers['Authorization'] = 'Bearer ' + LoginStore.access_token;
-  }
-  return fetchival(APIURL + url, _.extend(defOptions, options));
+    let defOptions = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors'
+    };
+    if (LoginStore.access_token && url != '/token') {
+        defOptions.headers['Authorization'] = 'Bearer ' + LoginStore.access_token;
+    }
+    return generate(APIURL + url, _.extend(defOptions, options));
+}
+
+function generate(url, options) {
+    let target = {};
+    let keys = ['post', 'delete', 'put', 'get', 'patch'].forEach((key) => {
+        target[key] = function(data) {
+            let args = [].slice.apply(arguments, []);
+            if(typeof(data) === 'object') {
+                if(key !== 'get') {
+                    args.shift();
+                    options.body = param(data);
+                } else {
+                  args.shift();
+                  args.unshift(_.pick(data, prop => typeof(prop) !== 'undefined'));
+                }
+            }
+            let request = fetchival(url, options);
+            return request[key].apply(request, args);
+        }
+    });
+    return target;
 }
--- src/scripts/util/util.js
+++ src/scripts/util/util.js
@@ -1,3 +1,7 @@
 export function param(obj) {
   return Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&');
 }
+
+export function always(promise, fn) {
+  return promise.then(fn, fn);
+}
