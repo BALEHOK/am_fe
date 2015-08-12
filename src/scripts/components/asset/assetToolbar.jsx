407e51ea6581436850a9a2b8889cbb8ea2f4ab38
Alexey Saenko <saenko.alexei@gmail.com>
2015-08-12 14:59:59 +0300

    fix buttons margin in asset sticky bar

--- src/scripts/components/asset/assetToolbar.jsx
+++ src/scripts/components/asset/assetToolbar.jsx
@@ -57,51 +57,46 @@ var AssetToolbar = React.createClass({
             <div className="asset-controls-panel-wrapper">
                 <div className="asset-controls-panel">
                     <div className="inputs-line inputs-line_width_full">
-                        {/*
-                         <button className="btn btn_type_second btn_size_small">
-                         <i className="btn__icon btn__icon_print"></i>
-                         </button>
-                         */}
                         {this.isCurrentButtonVisible()
                             ?   <button className="btn btn_type_second btn_size_small"
-                                        onClick={this.handleTransition.bind(this, 'asset-view')}>
-                            <i className="btn__icon btn__icon_history"></i>Current version
-                        </button>
-                            : ''
+                                                onClick={this.handleTransition.bind(this, 'asset-view')}>
+                                    <i className="btn__icon btn__icon_history"></i>Current version
+                                </button>
+                            :   null
                         }
 
                         {this.isHistoryButtonVisible()
                             ?   <button className="btn btn_type_second btn_size_small"
-                                        onClick={this.handleTransition.bind(this, 'asset-history')}>
-                            <i className="btn__icon btn__icon_history"></i>History
-                        </button>
-                            : ''
+                                                onClick={this.handleTransition.bind(this, 'asset-history')}>
+                                    <i className="btn__icon btn__icon_history"></i>History
+                                </button>
+                            :   null
                         }
 
                         {this.isEditButtonVisible()
-                            ? <button className="btn btn_type_second btn_size_small"
-                                      onClick={this.handleTransition.bind(this, 'asset-edit')}>
-                            <i className="btn__icon btn__icon_edit"></i>Edit
-                        </button>
-                            : ''
+                            ?   <button className="btn btn_type_second btn_size_small"
+                                              onClick={this.handleTransition.bind(this, 'asset-edit')}>
+                                    <i className="btn__icon btn__icon_edit"></i>Edit
+                                </button>
+                            :   null
                         }
 
                         {this.isDeleteButtonVisible()
-                            ? <button className='btn btn_type_warning btn_size_small pull-right'
+                            ?   <button className='btn btn_type_warning btn_size_small pull-right'
                                       onClick={this.handleAssetDeletion}>
-                            <i className='btn__icon btn__icon_cross'></i>
-                            Delete
-                        </button>
-                            : ''
+                                    <i className='btn__icon btn__icon_cross'></i>
+                                    Delete
+                                </button>
+                            :   null
                         }
 
                         {this.isRestoreButtonVisible()
-                            ? <button className='btn btn_type_second btn_size_small pull-right'
+                            ?   <button className='btn btn_type_second btn_size_small pull-right'
                                       onClick={this.handleAssetRestoration}>
-                            <i className='btn__icon btn__icon_undo'></i>
-                            Restore
-                        </button>
-                            : ''
+                                    <i className='btn__icon btn__icon_undo'></i>
+                                    Restore
+                                </button>
+                            :   null
                         }
                     </div>
                 </div>
