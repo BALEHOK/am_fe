66aff20e9142fd67b2ef4bd9fdd0b3df40c560f5
BALEHOK <zhmurik@yandex.ru>
2015-08-31 15:48:42 +0300

    AM-329 auth url

--- src/scripts/actions/LoginActions.js
+++ src/scripts/actions/LoginActions.js
@@ -1,18 +1,11 @@
 import Actions from './Actions'
 import RouterContainer from '../services/RouterContainer'
 import LoginDispatcher from '../dispatchers/LoginDispatcher'
+import AuthService from '../services/AuthService'
 
 export default {
   loginUser() {
-    let nextPath = '/';
-    let currentQuery = RouterContainer.get().getCurrentQuery();
-    if (currentQuery && currentQuery.nextPath) {
-      nextPath = currentQuery.nextPath;
-    }
-
     LoginDispatcher.loginUser();
-
-    RouterContainer.get().transitionTo(nextPath);
   },
 
   logoutUser(params) {
--- src/scripts/authCallback.js
+++ src/scripts/authCallback.js
@@ -1,3 +1,3 @@
 import AuthService from './services/AuthService'
 
-AuthService.handleCallback().then(() => window.location.href = '/');
+AuthService.handleCallback().then(() => window.location = window.location.protocol + "//" + window.location.host + '/');
