d83847c53d5b3ad409b2c0e10ad2158e49ce09c0
BALEHOK <zhmurik@yandex.ru>
2015-09-28 14:09:00 +0700

    AM-387 remember last path before login

--- src/scripts/mixins/AuthenticatedRouteMixin.js
+++ src/scripts/mixins/AuthenticatedRouteMixin.js
@@ -3,8 +3,13 @@ var LoginStore = require('../stores/LoginStore.js');
 var AuthenticatedRouteMixin = {
     statics: {
         willTransitionTo: function (transition, params, query) {
+            var nextPath;
             if (!LoginStore.store.isLoggedIn()) {
-               transition.redirect('/login', {}, {'nextPath' : transition.path});
+              localStorage['nextPath'] = transition.path;
+              transition.redirect('/login', {});
+            } else if (nextPath = localStorage['nextPath']) {
+              localStorage['nextPath'] = '';
+              transition.redirect(nextPath, {});
             }
         }
     }
