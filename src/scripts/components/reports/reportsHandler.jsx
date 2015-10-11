2390d05872ec293a8db1aa6615bdf25e35b40bc9
BALEHOK <zhmurik@yandex.ru>
2015-10-11 12:30:08 +0700

    AM-111 react-mixin fix

--- src/scripts/components/reports/reportsHandler.jsx
+++ src/scripts/components/reports/reportsHandler.jsx
@@ -1,23 +1,22 @@
 import React from 'react/addons'
 import reactMixin from 'react-mixin'
-import AuthenticatedRouteMixin from '../../mixins/AuthenticatedRouteMixin'
+import AuthenticatedMixin from '../../mixins/AuthenticatedRouteMixin'
 import ReportsList from './reportsList'
 import ReportActions from '../../actions/ReportActions'
 import ReportDispatcher from '../../dispatchers/ReportDispatcher'
 
-var ReportsHandler = React.createClass({
-    mixins: [AuthenticatedRouteMixin],
+@reactMixin.decorate(AuthenticatedMixin)
+export default class ReportsHandler extends React.Component {
 
-    componentWillMount: function() {
+    constructor(){
+        super();
         this.dispatcher = ReportDispatcher;
         this.actions = new ReportActions(this.dispatcher);
-    },
-
-    render: function() {
-        return (
-            <ReportsList actions={this.actions} dispatcher={this.dispatcher} />
-        );
     }
-});
 
-export default ReportsHandler;
+    render() {
+        return (
+                <ReportsList actions={this.actions} dispatcher={this.dispatcher} />
+            );
+    }
+}
