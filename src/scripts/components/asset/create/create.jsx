70d8b590c50b0ad7da01e2a724bcfa045011e91c
Alexey Saenko <saenko.alexei@gmail.com>
2015-10-30 15:49:34 +0300

    add grid component to "asset create" page

--- src/scripts/components/asset/create/create.jsx
+++ src/scripts/components/asset/create/create.jsx
@@ -1,13 +1,14 @@
-/**
- * @jsx React.DOM
- */
-
 var React = require('react');
+var FixedDataTable = require('fixed-data-table');
 var Flux = require('delorean').Flux;
 var Router = require('react-router');
+var moment = require('moment');
 var Loader = require('../../common/loader.jsx');
 var LoaderMixin = require('../../../mixins/LoaderMixin');
 var AssetTypeRow = require('./assetTypeRow');
+
+var Table = FixedDataTable.Table;
+var Column = FixedDataTable.Column;
 
 var Create = React.createClass({
     mixins:[Flux.mixins.storeListener, LoaderMixin],
@@ -18,43 +19,90 @@ var Create = React.createClass({
         router: React.PropTypes.func
     },
 
+    getInitialState: function() {
+        return {
+            gridMaxHeight: 600
+        };
+    },
+
     componentWillMount: function() {
         this.waitFor(this.props.actions.loadAssetTypes());
     },
 
-    onAssetTypeClick: function(assettype) {
+    componentDidUpdate: function(prevProps, prevState) {
+        if (prevState.loading && !this.state.loading) {
+            let gridNode = React.findDOMNode(this.refs.grid);
+            this.setState({
+                gridMaxHeight:  window.innerHeight - gridNode.getBoundingClientRect().top - 60
+            });
+        }
+    },
+
+    onRowClick: function(event, rowIndex) {
+        let id = this.state.stores.list.assettypes.activeTypes[rowIndex].id
         this.context.router.transitionTo(
             'asset-create-from-type',
-            {assetTypeId : assettype.id});
+            {assetTypeId : id}
+        );
+    },
+
+    rowGetter(rowIndex) {
+        return this.state.stores.list.assettypes.activeTypes[rowIndex];
+    },
+
+    renderDateCell(cellData) {
+        var date = moment(cellData);
+        return date.format("DD.MM.YYYY");
     },
 
     render: function() {
-        var rows = {};
         var assettypes = this.state.stores.list.assettypes;
-        if (assettypes && assettypes.activeTypes && assettypes.activeTypes.length > 0)
-         rows = assettypes.activeTypes.map(type => <AssetTypeRow type={type} onClick={this.onAssetTypeClick} />);
-      return (
-        <div>
-                <h1 className="page-title"><span className="icon icon_create"></span>New Asset</h1>
-                <h2>Please select an asset type</h2>
-                <Loader loading={this.state.loading}>
-                    <div className="grid asset-create">
+        return (
+            <Loader loading={this.state.loading}>
+                <div>
+                    <h1 className="page-title"><span className="icon icon_create"></span>New Asset</h1>
+                    <h2>Please select an asset type</h2>
+                    <div className="grid asset-create" ref="grid">
                         <div className="grid__item ten-twelfths">
-                            <table className="table">
-                                <tr>
-                                    <th width="25%">Name</th>
-                                    <th width="45%">Description</th>
-                                    <th width="12%">Revision</th>
-                                    <th width="13%">Date</th>
-                                    <th width="5%">&nbsp;</th>
-                                </tr>
-                                {rows}
-                            </table>
+                            {assettypes && assettypes.activeTypes && assettypes.activeTypes.length > 0
+                                ? <Table
+                                    rowHeight={50}
+                                    rowGetter={this.rowGetter}
+                                    rowsCount={assettypes.activeTypes.length}
+                                    width={1200}
+                                    maxHeight={this.state.gridMaxHeight}
+                                    headerHeight={50}
+                                    onRowClick={this.onRowClick}
+                                  >
+                                        <Column
+                                            label="Name"
+                                            width={300}
+                                            dataKey="displayName"
+                                        />
+                                        <Column
+                                            label="Description"
+                                            width={600}
+                                            dataKey="description"
+                                        />
+                                        <Column
+                                            label="Revision"
+                                            width={120}
+                                            dataKey="revision"
+                                        />
+                                        <Column
+                                            cellRenderer={this.renderDateCell}
+                                            label="Date"
+                                            width={180}
+                                            dataKey="updateDate"
+                                        />
+                                    </Table>
+                                : null
+                            }
                         </div>
                     </div>
-                </Loader>
-            </div>
-      );
+                </div>
+            </Loader>
+        );
     }
 });
 
