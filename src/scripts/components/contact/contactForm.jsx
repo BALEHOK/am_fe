e45c0cf9713b2d627a4c95e0a9ca3b94097fe0f1
Alexey Saenko <saenko.alexei@gmail.com>
2015-11-03 14:21:38 +0300

    test contacts api

--- src/scripts/components/contact/contactForm.jsx
+++ src/scripts/components/contact/contactForm.jsx
@@ -1,20 +1,29 @@
-/**
- * @jsx React.DOM
- */
 var React = require('react');
+import fetch from "../../util/fetch";
 
 var ContactForm = React.createClass({
 
+    onSubmit: function(e) {
+        e.preventDefault();
+        console.log('submit');
+        var subject = React.findDOMNode(this.refs.subject).value;
+        var message = React.findDOMNode(this.refs.message).value;
+        var response = fetch('/api/Contacts').post({
+             subject: subject,
+             message: message,
+        });
+    },
+
     render: function() {
         return (
-            <form action="" className="contact-form">
+            <form onSubmit={this.onSubmit} className="contact-form">
                 <label className="input-txt input-txt_width_full">
                     <span className="input-txt__title">Subject *</span>
-                    <input type="text" className="input-txt__field" required/>
+                    <input type="text" className="input-txt__field" ref="subject" required/>
                 </label>
                 <label className="input-txt input-txt_width_full">
                     <span className="input-txt__title">Message *</span>
-                    <textarea name="" id="" className="input-txt__field input-txt__field_area" required></textarea>
+                    <textarea name="" id="" className="input-txt__field input-txt__field_area" ref="message" required></textarea>
                 </label>
                 <div className="contact-form__controls txt-right">
                     <button className="btn">Send</button>
