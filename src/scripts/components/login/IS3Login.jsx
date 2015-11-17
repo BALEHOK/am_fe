aa63bba5e7505ac1fd141bcdf1b601abd83a7395
Ilya Bolkhovsky <ilya.bolkhovsky@gmail.com>
2015-11-17 12:18:40 +0300

    test clean up

--- src/scripts/components/login/IS3Login.jsx
+++ src/scripts/components/login/IS3Login.jsx
@@ -14,7 +14,7 @@ export default class Login extends React.Component {
     render() {
         return (
             <div className="auth-screen">
-                Logging in to #{AUTHURL}...
+                Logging in...
             </div>
         );
     }
