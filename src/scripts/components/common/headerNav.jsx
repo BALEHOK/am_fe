cad1aa9f6aac843cd4cdf07ed07995634dd826e8
Alexey Saenko <saenko.alexei@gmail.com>
2015-11-27 16:52:30 +0300

    update header nav

--- src/scripts/components/common/headerNav.jsx
+++ src/scripts/components/common/headerNav.jsx
@@ -15,10 +15,10 @@ var HeaderNav = React.createClass({
                     <ul className="header-nav__list">
                         <NavTab to="search"><TranslatedMessage messageId="search"/></NavTab>)
                         <NavTab to="reports"><TranslatedMessage messageId="reports"/></NavTab>
-                        <NavTab to="tasks">Tasks</NavTab>
+                        <NavTab to="tasks"><TranslatedMessage messageId="tasks"/></NavTab>
                         <NavTab to="asset-create"><TranslatedMessage messageId="assetCreate"/> <span className="icon icon_plus"></span></NavTab>
-                        <NavTab to="contact">Contact <span className="icon icon_mail"></span></NavTab>
-                        <NavTab to="faq">FAQ <span className="icon icon_question"></span></NavTab>
+                        <NavTab to="contact"><TranslatedMessage messageId="contact"/> <span className="icon icon_mail"></span></NavTab>
+                        <NavTab to="faq"><TranslatedMessage messageId="faq"/> <span className="icon icon_question"></span></NavTab>
                     </ul>
                 </div>
             </nav>
