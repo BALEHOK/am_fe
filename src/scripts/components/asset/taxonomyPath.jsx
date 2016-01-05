4a1fa88fa9f6d7784e2aa03163de9e46fe42335f
BALEHOK <zhmurik@yandex.ru>
2016-01-05 16:27:42 +0300

    show many catgories on asset view page #AM-463

--- src/scripts/components/asset/taxonomyPath.jsx
+++ src/scripts/components/asset/taxonomyPath.jsx
@@ -13,10 +13,13 @@ var TaxonomyLink = React.createClass({
         var taxonomy = this.props.taxonomy;
         return (
             <span>
-                {taxonomy.name}
-                &nbsp;<span className="icon icon_arrow_right"></span>&nbsp;
-                {taxonomy.child ? <TaxonomyLink taxonomy={taxonomy.child} /> : false}
-                {taxonomy.assetType ? taxonomy.assetType.displayName : false}
+                <span>
+                    {taxonomy.name}
+                    &nbsp;<span className="icon icon_arrow_right"></span>&nbsp;
+                    {taxonomy.child ? <TaxonomyLink taxonomy={taxonomy.child} /> : false}
+                    {taxonomy.assetType ? taxonomy.assetType.displayName : false}
+                </span>
+                <br/>
             </span>
         );
     }
@@ -24,10 +27,15 @@ var TaxonomyLink = React.createClass({
 
 var TaxonomyPath = React.createClass({
     render: function() {
+        var taxonomies = [];
+        if (this.props.taxonomyPath && this.props.taxonomyPath.length){
+            taxonomies = this.props.taxonomyPath.map(t => <TaxonomyLink taxonomy={t} />)
+        }
+
     	return <nav className="nav-block">
     			    <span className="nav-block__title nav-block__title_type_second">{L20nMessage('assetPageType', 'Asset type')}</span>
     			    <div className="nav-block__item">
-                        {this.props.taxonomyPath ? <TaxonomyLink taxonomy={this.props.taxonomyPath} /> : false}
+                        {taxonomies}
     			    </div>
     			</nav>
     }
